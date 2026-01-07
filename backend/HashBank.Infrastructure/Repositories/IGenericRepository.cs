deda2a0cc6335dd24c832d48495bdc91955e4bf7
Diego Molver <molverdiego@gmail.com>
2026-01-07 20:41:34 -0300

    Creo interfaz generica para repositorios

---
+++ backend/HashBank.Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using HashBank.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashBank.Infrastructure.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression);
+
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
