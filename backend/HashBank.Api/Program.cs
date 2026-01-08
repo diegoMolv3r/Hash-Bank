f86a5683527653379a908c34a5f052bd375c828c
Diego Molver <molverdiego@gmail.com>
2026-01-07 21:54:54 -0300

    Instalacion y configuracion de Entity Framework (code first)

--- backend/HashBank.Api/Program.cs
+++ backend/HashBank.Api/Program.cs
@@ -1,14 +1,24 @@
+using HashBank.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<HashBankDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("HashBank.Infrastructure")
+    ));
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
---
+++ backend/HashBank.Infrastructure/Persistence/HashBankDbContext.cs
@@ -0,0 +1,25 @@
+﻿using HashBank.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HashBank.Infrastructure.Persistence
+{
+    public class HashBankDbContext : DbContext
+    {
+        public HashBankDbContext(DbContextOptions<HashBankDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(p => p.Balance)
+                .HasPrecision(18, 4);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
