d8dd6d02f9381192d83d2b48224f642382d9da2d
Diego Molver <molverdiego@gmail.com>
2026-01-10 19:39:09 -0300

    Agrego Middleware para evitar errores en la conexion con angular

--- backend/HashBank.Api/Program.cs
+++ backend/HashBank.Api/Program.cs
@@ -15,6 +15,17 @@ builder.Services.AddDbContext<HashBankDbContext>(options =>
         b => b.MigrationsAssembly("HashBank.Infrastructure")
     ));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -41,6 +52,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAngular");
 
 app.MapControllers();
 
