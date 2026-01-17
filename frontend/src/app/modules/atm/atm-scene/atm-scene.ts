c280e1c73feebf7424720cd35d25b8cdff732bb5
Diego Molver <molverdiego@gmail.com>
2026-01-17 14:45:57 -0300

    Ajusto cordenadas en X para el zoom a la pantalla

--- frontend/src/app/modules/atm/atm-scene/atm-scene.ts
+++ frontend/src/app/modules/atm/atm-scene/atm-scene.ts
@@ -32,30 +32,28 @@ export class AtmSceneComponent implements AfterViewInit, OnDestroy {
     const height = canvas.parentElement?.clientHeight || window.innerHeight;
 
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x1a1a2e); // Color azul oscuro estilo "banco seguro"
+    this.scene.background = new THREE.Color(0x1a1a2e);
 
-    // 2. CÁMARA
-    // poner la perspectiva inicial en el centro 
+    // CÁMARA
     this.camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
-    this.camera.position.set(-0.33, 21.41, 34.43); // Posición inicial x y z
+    this.camera.position.set(-0.33, 21.41, 34.43);
    
 
-    // 3. RENDERER
+    // RENDERER
     this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
     this.renderer.setSize(width, height);
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.shadowMap.enabled = true; // Activar sombras
+    this.renderer.shadowMap.enabled = true;
 
     this.renderer.outputColorSpace = THREE.SRGBColorSpace;
     this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
     this.renderer.toneMappingExposure = 0.5;
 
-    // 4. LUCES
+    // LUCES
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
     this.scene.add(ambientLight);
 
-    // Bajar la luz direccional (el sol)
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5); // <--- Estaba en 1
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
     directionalLight.position.set(5, 10, 7);
     directionalLight.castShadow = true;
     this.scene.add(directionalLight);
@@ -64,40 +62,24 @@ export class AtmSceneComponent implements AfterViewInit, OnDestroy {
     pmremGenerator.compileEquirectangularShader();
     this.scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
 
-
-    // 5. CONTROLES (Para rotar con el mouse mientras desarrollas)
+    // CONTROLES 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.enableDamping = true; // Movimiento suave
-
+    this.controls.enableDamping = true; 
     this.controls.target.set(-1.86, 10.51, -2.79);
 
     this.controls.update();
-
-    // INICIAL
-    // Cámara (Position): x: -0.33, y: 21.41, z: 34.43
-    // Mira a (Target):   x: -1.86, y: 10.51, z: -2.79
-
-    // ZOOM AL CAJERO
-    // Cámara (Position): x: -1.34, y: 20.73, z: 12.95
-    // Mira a (Target):   x: -1.10, y: 15.62, z: -4.50
-
-    // ZOOM A LA PANTALLA
-    // Cámara (Position): x: -1.48, y: 19.68, z: 6.02
-    // Mira a (Target):   x: -1.70, y: 16.32, z: -4.34
   }
 
   private loadModel(): void {
     const loader = new GLTFLoader();
     
-    // Ruta del modelo GLB
     loader.load('/assets/models/cajero.glb', (gltf: any) => {
       const model = gltf.scene;
       
-      // Ajustes comunes al modelo
       model.position.set(0, 0, 0); 
-      model.scale.set(1, 1, 1); // Juega con esto si el modelo es muy grande o chico
+      model.scale.set(1, 1, 1); 
       
-      // Activar sombras en el modelo
+      // Sombras
       model.traverse((child: any) => {
         if (child.isMesh) {
           child.castShadow = true;
@@ -114,7 +96,6 @@ export class AtmSceneComponent implements AfterViewInit, OnDestroy {
   }
 
   private animate(): void {
-    // Loop de renderizado (se ejecuta 60 veces por segundo)
     this.animationId = requestAnimationFrame(() => this.animate());
     
     if (this.controls) this.controls.update();
@@ -185,8 +166,8 @@ export class AtmSceneComponent implements AfterViewInit, OnDestroy {
   }
 
   public zoomToScreenDeep(): void {
-  const deepTarget = new THREE.Vector3(0, 17, -3); 
-  const deepCamera = new THREE.Vector3(0, 19, 5);
+  const deepTarget = new THREE.Vector3(-0.8, 17, -3); 
+  const deepCamera = new THREE.Vector3(-0.8 , 19, 5);
 
   gsap.to(this.camera.position, {
     x: deepCamera.x, y: deepCamera.y, z: deepCamera.z,
@@ -238,21 +219,6 @@ export class AtmSceneComponent implements AfterViewInit, OnDestroy {
   
 
   ngOnDestroy(): void {
-    // Limpieza para evitar fugas de memoria
     if (this.animationId) cancelAnimationFrame(this.animationId);
-  }
-
-  public logCoordinates(): void {
-  console.log('--- COPIA ESTOS NÚMEROS ---');
-  
-  // Posición de la cámara (dónde está parada)
-  const pos = this.camera.position;
-  console.log(`Cámara (Position): x: ${pos.x.toFixed(2)}, y: ${pos.y.toFixed(2)}, z: ${pos.z.toFixed(2)}`);
-  
-  // Punto de mira (hacia dónde está mirando)
-  const target = this.controls.target;
-  console.log(`Mira a (Target):   x: ${target.x.toFixed(2)}, y: ${target.y.toFixed(2)}, z: ${target.z.toFixed(2)}`);
-  
-  console.log('---------------------------');
   }
 }
